2e8525ddca7b725b90f79aa07229099edd3c68bc
nidaa <nida.alk94@gmail.com>
2021-01-11 10:04:17 +0200

    some changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 function goHomePg() {
     location.href = 'page14.html';
 }
+
+function getLangJSON(lang) {
+    $.getJSON('js/lang.json', function (response) {
+        localStorage.clear();
+        localStorage.setItem("jsonLang", JSON.stringify(response));
+        JsonLang = localStorage.getItem("jsonLang");
+        console.log(JsonLang);
+    });
+}
