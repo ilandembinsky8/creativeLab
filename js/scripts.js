0d4598c907c673f7f6c435dd0762b385fabbfc2e
nidaa <nida.alk94@gmail.com>
2021-01-13 14:00:34 +0200

    some changes

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,10 @@ function mail() {
     window.location = "mailto:";
 }
 
+function swapStyleSheet(sheet) {
+    document.getElementById("pagestyle").setAttribute("href", sheet);
+}
+
 function getLangJSON(lang) {
     $.getJSON('js/lang.json', function (response) {
         localStorage.clear();
