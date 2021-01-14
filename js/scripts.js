bbe54afbeaf684e4d99834f73b796916f5c17379
nidaa <nida.alk94@gmail.com>
2021-01-14 10:28:01 +0200

    some changes

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,32 @@ function getLangJSON(lang) {
         console.log(JsonLang);
     });
 }
+
+
+function loadLang() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = JSON.parse(this.responseText);
+            var pickLang = localStorage.getItem("langID");
+            console.log(myObj);
+            if (typeof pickLang !== 'undefined' && pickLang !== null) {
+                if (pickLang.localeCompare("he") === 0) {
+                    console.log("to hebrew");
+                    localStorage.setItem("langID", "he");
+                    localStorage.setItem("lang", JSON.stringify(myObj.HE));
+                } else {
+                    console.log("to english");
+                    localStorage.setItem("langID", "en");
+                    localStorage.setItem("lang", JSON.stringify(myObj.EN));
+                }
+            } else {
+                localStorage.setItem("langID", "he");
+                localStorage.setItem("lang", JSON.stringify(myObj.HE));
+            }
+        }
+    };
+    xmlhttp.open("GET", "js/lang.json", true);
+    xmlhttp.send();
+
+}
